cad1f8305d0f358ef083bac64c97f0f9dbda5325
Trevor Jex <jextrevor@gmail.com>
2015-02-21 09:45:41 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 from flask import Flask,render_template,request,redirect,url_for
 from flask.ext.socketio import SocketIO, emit, join_room, leave_room
 import urllib
