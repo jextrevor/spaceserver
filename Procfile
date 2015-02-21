c1fdcde4278aa60965f9fd0e6587ea04015e0489
Trevor Jex <jextrevor@gmail.com>
2015-02-21 13:27:50 -0700

    Updated for dotcloud

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python2.7 main.py
--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-#!/usr/bin/python
-import os
-
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
 from flask import Flask,render_template,request,redirect,url_for
 from flask.ext.socketio import SocketIO, emit, join_room, leave_room
 import urllib
-application = Flask(__name__)
-socketio = SocketIO(application)
+import os
+app = Flask(__name__)
+socketio = SocketIO(app)
 dictionary = {}
 @socketio.on('connect', namespace="/commander")
 def stationconnect():
@@ -38,66 +26,66 @@ def stationconnect():
 @socketio.on('connect', namespace="/fd")
 def stationconnect():
     emit("update",dictionary)
-@application.route('/')
+@app.route('/')
 def home():
     templateData = {
         }
     return render_template('main.html', **templateData)
-@application.route("/commander/", methods=['POST'])
+@app.route("/commander/", methods=['POST'])
 def commander():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("commander.html", **templateData)
-@application.route("/navigator/", methods=['POST'])
+@app.route("/navigator/", methods=['POST'])
 def navigator():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("navigator.html", **templateData)
-@application.route("/tactical/", methods=['POST'])
+@app.route("/tactical/", methods=['POST'])
 def tactical():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("tactical.html", **templateData)
-@application.route("/operations/", methods=['POST'])
+@app.route("/operations/", methods=['POST'])
 def operations():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("operations.html", **templateData)
-@application.route("/engineer/", methods=['POST'])
+@app.route("/engineer/", methods=['POST'])
 def engineer():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("engineer.html", **templateData)
-@application.route("/mvs/", methods=['POST'])
+@app.route("/mvs/", methods=['POST'])
 def mvs():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("mvs.html", **templateData)
-@application.route("/fd/", methods=['POST'])
+@app.route("/fd/", methods=['POST'])
 def fd():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("fd.html", **templateData)
-@application.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(error):
     templateData = {
         'error':"Page Not Found"
         }
     return render_template("error.html",**templateData)
-@application.errorhandler(500)
+@app.errorhandler(500)
 def internalerror(error):
     templateData = {
         'error':error
         }
     return render_template("error.html",**templateData)
-@application.after_request
+@app.after_request
 def no_cache(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'no-cache, no-store'
@@ -105,4 +93,4 @@ def no_cache(response):
     return response
 if __name__ == '__main__':
     print "Server running"
-    socketio.run(application, "0.0.0.0",8051)
+    socketio.run(app, "0.0.0.0",int(os.environ['PORT']))
