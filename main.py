dd1af196271555fcf2d61a92057b7f732ecc9daf
Trevor Jex <jextrevor@gmail.com>
2015-02-21 13:05:10 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ except IOError:
 from flask import Flask,render_template,request,redirect,url_for
 from flask.ext.socketio import SocketIO, emit, join_room, leave_room
 import urllib
-app = Flask(__name__)
-socketio = SocketIO(app)
+application = Flask(__name__)
+socketio = SocketIO(application)
 dictionary = {}
 @socketio.on('connect', namespace="/commander")
 def stationconnect():
@@ -38,66 +38,66 @@ def stationconnect():
 @socketio.on('connect', namespace="/fd")
 def stationconnect():
     emit("update",dictionary)
-@app.route('/')
+@application.route('/')
 def home():
     templateData = {
         }
     return render_template('main.html', **templateData)
-@app.route("/commander/", methods=['POST'])
+@application.route("/commander/", methods=['POST'])
 def commander():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("commander.html", **templateData)
-@app.route("/navigator/", methods=['POST'])
+@application.route("/navigator/", methods=['POST'])
 def navigator():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("navigator.html", **templateData)
-@app.route("/tactical/", methods=['POST'])
+@application.route("/tactical/", methods=['POST'])
 def tactical():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("tactical.html", **templateData)
-@app.route("/operations/", methods=['POST'])
+@application.route("/operations/", methods=['POST'])
 def operations():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("operations.html", **templateData)
-@app.route("/engineer/", methods=['POST'])
+@application.route("/engineer/", methods=['POST'])
 def engineer():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("engineer.html", **templateData)
-@app.route("/mvs/", methods=['POST'])
+@application.route("/mvs/", methods=['POST'])
 def mvs():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("mvs.html", **templateData)
-@app.route("/fd/", methods=['POST'])
+@application.route("/fd/", methods=['POST'])
 def fd():
 	templateData = {
 		'name':request.form.get("name")
 	}
 	return render_template("fd.html", **templateData)
-@app.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(error):
     templateData = {
         'error':"Page Not Found"
         }
     return render_template("error.html",**templateData)
-@app.errorhandler(500)
+@application.errorhandler(500)
 def internalerror(error):
     templateData = {
         'error':error
         }
     return render_template("error.html",**templateData)
-@app.after_request
+@application.after_request
 def no_cache(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'no-cache, no-store'
@@ -105,4 +105,4 @@ def no_cache(response):
     return response
 if __name__ == '__main__':
     print "Server running"
-    socketio.run(app, "0.0.0.0",8051)
+    socketio.run(application, "0.0.0.0",8051)
