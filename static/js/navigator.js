8e07dd425b26688735062bea07d252a500ed4ffa
jextrevor <jextrevor@gmail.com>
2015-02-24 17:08:15 -0700

    Update navigator.js

--- static/js/navigator.js
+++ static/js/navigator.js
@@ -4,18 +4,15 @@ var conn_options = {
 data = {}
 var socket = io.connect('http://'+window.location.hostname+':'+window.location.port+'/navigator',conn_options);
 socket.on("update",function(json){
+	update(json);
+}
+});
+function emit(key,json){
+	socket.emit(key,json);
+}
+function update(json){
 	for (var key in json) {
   if (json.hasOwnProperty(key)) {
     data[key] = json[key];
   }
 }
-});
-var peer = new Peer("navigator",{key: 'x7imbejnpg2pgb9'});
-var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-peer.on('call', function(call) {
-  getUserMedia({video: true, audio: true}, function(stream) {
-    call.answer(stream); // Answer the call with an A/V stream.
-  }, function(err) {
-    console.log('Failed to get local stream' ,err);
-  });
-});
