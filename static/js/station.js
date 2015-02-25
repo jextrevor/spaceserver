97f23c3d2d2420fb5e7c58341323ea508567ec84
jextrevor <jextrevor@gmail.com>
2015-02-24 17:15:34 -0700

    Update and rename static/js to static/js/station.js

---
+++ static/js/station.js
@@ -0,0 +1,16 @@
+function buttonsound(){
+    var data = new Audio("static/media/button.wav");
+    data.play();
+}
+function shortsound(){
+    var data = new Audio("static/media/buttonshort.wav");
+    data.play();
+}
+function processsound(){
+    var data = new Audio("static/media/processing.mp3");
+    data.play();
+}
+function infosound(){
+    var data = new Audio("static/media/info.wav");
+    data.play();
+}
