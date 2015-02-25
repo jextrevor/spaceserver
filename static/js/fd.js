ef1ec58e86a19b6d508999d23072f1d1a4f5f901
jextrevor <jextrevor@gmail.com>
2015-02-24 17:07:33 -0700

    Update fd.js

--- static/js/fd.js
+++ static/js/fd.js
@@ -4,42 +4,27 @@ var conn_options = {
 data = {}
 var socket = io.connect('http://'+window.location.hostname+':'+window.location.port+'/fd',conn_options);
 socket.on("update",function(json){
-	for (var key in json) {
+	update(json);
+}
+});
+function emit(key,json){
+socket.emit(key,json);
+}
+function update(json){
+for (var key in json) {
   if (json.hasOwnProperty(key)) {
     data[key] = json[key];
     if(key == "missiondata"){
     	document.getElementById("missiondata").value = json[key];
     }
+    if(key == "x"){
+    	document.getElementById("x").value = json[key];
+    }
+    if(key == "y"){
+    	document.getElementById("y").value = json[key];
+    }
+    if(key == "z"){
+    	document.getElementById("z").value = json[key];
+    }
   }
-}
-});
-var peer1 = new Peer({key: 'x7imbejnpg2pgb9'});
-var peer2 = new Peer({key: 'x7imbejnpg2pgb9'});
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-function call(peerid){
-if(window.existingcall){
-window.existingcall.close();
-}
-navigator.getUserMedia({audio: true, video: true}, function(stream){
-// Set your video displays
-//document.getElementById("yourvideo").src = URL.createObjectURL(stream);
-window.localStream = stream;
-},function(err){console.log("no!!!")});
-var call = peer1.call(peerid, window.localStream);
-call.on('stream', function(remoteStream) {
-      document.getElementById("theirvideo").src = URL.createObjectURL(stream);
-    });
-window.existingcall = call;
-}
-function hail(peerid){
-if(window.existinghail){
-window.existinghail.close();
-}
-navigator.getUserMedia({audio: data['fdaudio'], video: data['fdvideo']}, function(stream){
-// Set your video displays
-document.getElementById("yourvideo").src = URL.createObjectURL(stream);
-window.hailStream = stream;
-});
-var call = peer2.call(peerid, window.hailStream);
-window.existinghail = call;
 }
