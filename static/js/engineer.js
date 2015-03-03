6cc3b0a0ebd4ac7114fdab83ed014eb4c92ae8eb
Trevor Jex <jextrevor@gmail.com>
2015-03-02 20:28:49 -0700

    Started on Engineer

--- static/js/engineer.js
+++ static/js/engineer.js
@@ -3,6 +3,7 @@ var conn_options = {
 'transports':['flashsocket','htmlfile','xhr-polling','jsonp-polling','websocket']
 };
 data = {}
+var events;
 var socket = io.connect('http://'+window.location.hostname+':'+window.location.port+'/engineer',conn_options);
 socket.on("update",function(json){
 	update(json);
@@ -15,6 +16,34 @@ function update(json){
 	for (var key in json) {
   if (json.hasOwnProperty(key)) {
     data[key] = json[key];
+    if(key == "coolant"){
+    	document.getElementById("coolant").innerHTML = json[key];
+    	    }
   }
 }
 }
+function showengines(){
+	document.getElementById("enginespage").style.display = "initial";
+	document.getElementById("radarpage").style.display = "none";
+	document.getElementById("settingspage").style.display = "none";
+	document.getElementById("enginestab").className = "active";
+	document.getElementById("radartab").className = "";
+	document.getElementById("settingstab").className = "";
+}
+function showradar(){
+	document.getElementById("enginespage").style.display = "none";
+	document.getElementById("radarpage").style.display = "initial";
+	document.getElementById("settingspage").style.display = "none";
+	document.getElementById("enginestab").className = "";
+	document.getElementById("radartab").className = "active";
+	document.getElementById("settingstab").className = "";
+}
+function showsettings(){
+	document.getElementById("enginespage").style.display = "none";
+	document.getElementById("radarpage").style.display = "none";
+	document.getElementById("settingspage").style.display = "initial";
+	document.getElementById("enginestab").className = "";
+	document.getElementById("radartab").className = "";
+	document.getElementById("settingstab").className = "active";
+}
+
