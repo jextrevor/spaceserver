43708b88d610832b5c28d7179e79482e1c534bc3
Trevor Jex <jextrevor@gmail.com>
2015-03-23 17:51:18 -0600

    Added speech capabilities

--- static/js/speakWorker.js
+++ static/js/speakWorker.js
@@ -1,4 +1,4 @@
-importScripts('speakGenerator.js');
+importScripts('/static/js/speakGenerator.js');
 
 onmessage = function(event) {
   postMessage(generateSpeech(event.data.text, event.data.args));
