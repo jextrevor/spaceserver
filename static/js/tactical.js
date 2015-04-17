0c1f6e28ad29f8070fc4d005fc651830473899b1
jextrevor <jextrevor@gmail.com>
2015-04-17 11:28:06 -0600

    Update tactical.js

--- static/js/tactical.js
+++ static/js/tactical.js
@@ -1,6 +1,5 @@
 var conn_options = {
 'sync disconnect on unload':true,
-'transports':['flashsocket','htmlfile','xhr-polling','jsonp-polling','websocket']
 };
 data = {}
 var socket = io.connect('http://'+window.location.hostname+':'+window.location.port+'/tactical',conn_options);
